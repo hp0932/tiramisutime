96e92d5cc621f8af18ea8b6be7fbcf3f54e3f8c1
beans <hp0932@gmail.com>
2020-05-14 10:20:17 +0900

    파일 다운로더 작성중

--- custard/src/main/java/com/custard/service/FileviewService.java
+++ custard/src/main/java/com/custard/service/FileviewService.java
@@ -1,21 +1,34 @@
 package com.custard.service;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.TrueFileFilter;
+import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.custard.controller.FileviewController;
+import com.custard.dto.FileDto;
 
 @Service
 public class FileviewService {
+	
+	@Autowired
+	ModelMapper modelMapper;
 	
 	private static final Logger logger = LoggerFactory.getLogger(FileviewService.class);
 	private static final String ROUTE = "D:\\workshop";
@@ -53,6 +66,71 @@ public class FileviewService {
         
 		result.put("fileList", fileList);
 		return result;
+	}
+	
+	/**
+	 * 파일 다운로드
+	 * @param request
+	 * @param response
+	 */
+	public void getFile(HttpServletRequest request, HttpServletResponse response) {
+		
+		boolean lost = false;
+		File file = null;
+		InputStream in = null;
+		OutputStream os = null;
+		String fileName = request.getParameter("fileName");
+		
+		try {
+			try {
+				file = new File(ROUTE, fileName);
+				in = new FileInputStream(file);
+			} catch (FileNotFoundException e) {
+				logger.debug("§§§ file lost §§§");
+				lost = true;
+			}
+			String client = request.getHeader("User-Agent");
+			response.reset();
+			response.setContentType("application/octet-stream");
+			response.setHeader("Content-Description", "JSP Generated Data");
+			
+			//파일이 분실되지 않았다면
+			if(!lost) {
+				// IE
+				if(client.indexOf("MSIE") != -1){
+					response.setHeader ("Content-Disposition", "attachment; filename="+new String(fileName.getBytes("KSC5601"),"ISO8859_1"));
+					
+				}else{
+					// 한글 파일명 처리
+					fileName = new String(fileName.getBytes("utf-8"),"iso-8859-1");
+					
+					response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+					response.setHeader("Content-Type", "application/octet-stream; charset=utf-8");
+				} 
+				
+				response.setHeader ("Content-Length", ""+file.length() );
+				
+				
+				
+				os = response.getOutputStream();
+				byte b[] = new byte[8192];
+				int leng = 0;
+				int limitSize = 0;
+				while( (leng = in.read(b)) > 0 ){
+					os.write(b,0,leng);
+					limitSize += leng;
+					if(limitSize > 1024 * 1024) {
+						limitSize = 0;
+						os.flush();
+					}
+				}
+			}
+			
+		in.close();
+		os.close();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public String fileSizeMaker(Long fileSize) {
