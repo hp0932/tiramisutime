64229eb03b5dc9119a55d0dfdbfbd48a30eb5c5e
beans <hp0932@gmail.com>
2020-07-01 10:20:56 +0900

    interceptor 추가

---
+++ custard/src/main/java/com/custard/configuration/LoggerInterceptor.java
@@ -0,0 +1,28 @@
+package com.custard.configuration;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+@Configuration
+public class LoggerInterceptor extends HandlerInterceptorAdapter  {
+	
+	private static final Logger logger = LoggerFactory.getLogger(LoggerInterceptor.class);
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		logger.debug("Request URI ===> " + request.getRequestURI());
+		return super.preHandle(request, response, handler);
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+		//logger.debug("==================== END ======================");
+	}
+	
+}
